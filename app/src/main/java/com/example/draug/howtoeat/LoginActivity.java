690973e2864fe2d62289bdaad52a45b8b838d393
Steve Rivieccio <draugvar@gmail.com>
2015-08-22 18:49:24 +0200

    last

--- app/src/main/java/com/example/draug/howtoeat/LoginActivity.java
+++ app/src/main/java/com/example/draug/howtoeat/LoginActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void clickLogin(View view){
+        EditText textEmail = (EditText) findViewById(R.id.editTextEmail);
+        EditText textPassword = (EditText) findViewById(R.id.editTextPassword);
 
+
+    }
 }
