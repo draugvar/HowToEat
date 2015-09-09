b6f3bbadf7469b1f946e83a1e8c109227c9555fb
Steve Rivieccio <draugvar@gmail.com>
2015-09-09 11:10:21 +0200

    last

--- app/src/main/java/com/example/draug/howtoeat/HomeActivity.java
+++ app/src/main/java/com/example/draug/howtoeat/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.example.draug.howtoeat;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -10,8 +9,6 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,9 +22,6 @@ public class HomeActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,20 +79,6 @@ public class HomeActivity extends AppCompatActivity {
         username.setText(sharedPref.getString("userEmail", "username"));
         email.setText(sharedPref.getString("userEmail", "username"));
 
-        /*//set recycler
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);*/
     }
 
     /* Called whenever we call invalidateOptionsMenu() */
